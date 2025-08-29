2628680a6e6b589c551fe6381cb59de82b6812fc
farrahpepino <idcfarrah@gmail.com>
2025-08-29 19:37:32 -0400

    improve ui

--- server/Repositories/OrderRepository.cs
+++ server/Repositories/OrderRepository.cs
@@ -1,28 +1,28 @@
-using server.Data;
-using Dapper;
-using server.Dtos;
+// using server.Data;
+// using Dapper;
+// using server.Dtos;
 
-namespace server.Repositories{
-    public class OrderRepository {
-        private readonly DapperContext _context;
+// namespace server.Repositories{
+//     public class OrderRepository {
+//         private readonly DapperContext _context;
 
-        public DapperContext(DapperContext context){
-            _context = context;
-        }
+//         public DapperContext(DapperContext context){
+//             _context = context;
+//         }
 
-        private const string InsertOrderQuery = @"INSERT INTO Orders (Items, OrderId, OrderedAt, CustomerId) VALUES (@Items, @OrderId, @OrderedAt, @CustomerId)";
-        // have to think about the items 
+//         private const string InsertOrderQuery = @"INSERT INTO Orders (Items, OrderId, OrderedAt, CustomerId) VALUES (@Items, @OrderId, @OrderedAt, @CustomerId)";
+//         // have to think about the items 
     
-        public async Task CreateOrder(OrderDto order){
-            var connection = await _context.CreateConnection();
-            return await connection.ExecuteAsync(InsertOrderQuery, order);
-        }
+//         public async Task CreateOrder(OrderDto order){
+//             var connection = await _context.CreateConnection();
+//             return await connection.ExecuteAsync(InsertOrderQuery, order);
+//         }
 
-        public async Task<OrderDto> GetOrder (string OrderId){
-            var connection = await _context.CreateConnection();
-            return await connection.QueryFirstOrDefaultAsync(SelectOrderQuery, new {OrderId = OrderId});
-        }
+//         public async Task<OrderDto> GetOrder (string OrderId){
+//             var connection = await _context.CreateConnection();
+//             return await connection.QueryFirstOrDefaultAsync(SelectOrderQuery, new {OrderId = OrderId});
+//         }
 
 
-    }
-}
+//     }
+// }
--- server/Services/OrderService.cs
+++ server/Services/OrderService.cs
@@ -1,15 +1,15 @@
-using server.Dtos;
-using server.Controllers;
-using server.Repositories;
+// using server.Dtos;
+// using server.Controllers;
+// using server.Repositories;
 
-namespace server.Services{
-    public class OrderService{
-        private readonly OrderRepository _orderRepository;
+// namespace server.Services{
+//     public class OrderService{
+//         private readonly OrderRepository _orderRepository;
 
-        public OrderService (OrderRepository orderRepository){
-            _orderRepository = orderRepository;
-        }
+//         public OrderService (OrderRepository orderRepository){
+//             _orderRepository = orderRepository;
+//         }
 
-        // public Task
-    }
-}
+//         // public Task
+//     }
+// }
